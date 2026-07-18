340371b247f7396683307c469fd201f109b2794b
FABRIFAMAF <fabrizio.peralta@mi.unc.edu.ar>
2026-07-18 20:57:18 -0300

    Mas implementaciones de seguridad y cierre de sesion

---
+++ src/main/java/com/fabri/taco/Security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.fabri.taco.Security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fabri.taco.Repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return userRepository.findByUsername(username)
+            .orElseThrow(() ->
+                new UsernameNotFoundException(
+                    "Usuario no encontrado: " + username
+                )
+            );
+    }
+}
--- src/main/java/com/fabri/taco/Security/SecurityConfig.java
+++ src/main/java/com/fabri/taco/Security/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.fabri.taco.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,4 +15,25 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+
+        return http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/design/**", "/orders/**")
+                    .hasRole("USER")
+                .anyRequest()
+                    .permitAll()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/")
+                .permitAll()
+            )
+            .build();
+    }
 }
